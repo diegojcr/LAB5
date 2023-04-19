65ac08add37198695249677f8406ec46d7ae273a
Sebastian <u.cuevas.5ddb@gmail.com>
2023-04-18 22:42:49 -0600

    Carga de archivos

---
+++ LAB5/LAB5/Controllers/AgregarManual.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LAB5.Controllers
+{
+    public class AgregarManual : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
